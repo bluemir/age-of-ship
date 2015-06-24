a8be4b7b7a186a69b59bec811e53bf30f240646b
BlueMir <qa22ahj@naver.com>
2015-06-25 05:43:44 +0900

    Add sun

---
+++ assets/entity/sun.js
@@ -0,0 +1,16 @@
+define(["scene"], function(scene){
+	function Sun(){
+		var ambient = new T.AmbientLight(0xffffff);
+		ambient.color.setHSL(0.1, 0.3, 0.2);
+
+		var directional = new T.DirectionalLight( 0xffffff, 0.5 );
+		directional.position.set( 0, 1, 0 );
+
+		this.mesh = new T.Object3D();
+		this.mesh.add(ambient);
+		this.mesh.add(directional);
+		
+		scene.add(this.mesh);
+	}
+	return Sun;
+})
