b108615845287648ec890fb859109919698b3be9
BlueMir <qa22ahj@naver.com>
2015-09-24 00:34:19 +0900

    Add index page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var swig = require("swig");
 var app = express();
 
+var PORT = process.env.PORT || 3003;
+
 app.engine("html", swig.renderFile);
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
@@ -10,12 +12,12 @@ app.use("/lib", express.static("bower_components"));
 app.use("/assets", express.static("assets"))
 
 app.get("/", function(req, res){
-	res.send("hello");
+	res.render("index.html");
 });
 app.get("/game", function(req, res){
 	res.render("game.html");
 });
 
-var server = app.listen(3003, function() {
-	console.log("start server");
+var server = app.listen(PORT, function() {
+	console.log("start server @ ", PORT);
 });
